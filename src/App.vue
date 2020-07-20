b22c041fa934d6ccea2735537606b2513477b666
yboodhan <yboodhan@gmail.com>
2020-07-20 14:54:08 -0700

    Successfully passed and updated prop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
+    <p v-if="gems">{{ gems[0].name }} </p>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" v-bind:gems.sync="gems"/>
   </div>
 </template>
 
@@ -12,6 +13,27 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      gems: null
+    };
+  },
+  async beforeMount() {
+      try {
+        let jsonData = await require("./assets/jewel_data.json");
+        if (!jsonData) {
+          console.log("No data could be found.");
+          this.gems = [];
+        } else {
+          console.log("The following data was found.");
+          console.log(jsonData);
+          this.gems = jsonData;
+        }
+      } catch (err) {
+        console.log("We got an error", err);
+        this.gems = [];
+      }
   }
 }
 </script>
